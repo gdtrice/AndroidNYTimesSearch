c9387908c383897288e194f96b92cbdd5d0cd574
Garland Trice <GDTrice@hotmail.com>
2016-08-12 01:06:44 -0700

    initial commit

---
+++ app/src/main/java/com/example/gtrice/nytimessearch/Models/Article.java
@@ -0,0 +1,60 @@
+package com.example.gtrice.nytimessearch.Models;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by gtrice on 8/9/16.
+ */
+public class Article  implements Serializable {
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getThumbnail() {
+        return String.format("http://www.nytimes.com/%s", thumbnail);
+    }
+
+    String webUrl;
+    String headline;
+    String thumbnail;
+
+    public Article(JSONObject jsonObject){
+        try {
+            this.webUrl = jsonObject.getString("web_url");
+            this.headline = jsonObject.getJSONObject("headline").getString("main");
+
+            JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+            if (multimedia.length() > 0) {
+                JSONObject multimediaJson = multimedia.getJSONObject(0);
+                this.thumbnail = multimediaJson.getString("url");
+            } else {
+                this.thumbnail = "";
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Article> fromJSONArray(JSONArray array) {
+        ArrayList<Article> results = new ArrayList<>();
+        for (int i=0; i < array.length(); i++) {
+            try {
+                results.add(new Article(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
